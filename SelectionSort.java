c50e0c6a62040c876692c6a74badccfa84893db7
afreenfquazi <95732654+afreenfquazi@users.noreply.github.com>
2022-11-18 17:12:25 -0600

    Add files via upload

--- SelectionSort.java
+++ SelectionSort.java
@@ -4,18 +4,18 @@ public class SelectionSort {
 
 	public static void main(String[] args) {
 		int arr[] = { 54, 23, 67, 12,9,32,87,2,90,14,56,1,43,24};
-		int temp=0;
+		
 		int k = 0;
 		while(k<arr.length)
 		{
-		for(int i=0;i< arr.length;i++)
+		for(int i=0;i< arr.length-1;i++)
 		{
 			for(int j=i+1;j< arr.length;)
 			{
 				if(arr[i]>arr[j]) {
-					temp = arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;
+					arr[i] = arr[i]+arr[j];
+					arr[j]=arr[i]-arr[j];
+					arr[i]=arr[i]-arr[j];
 					break;
 					
 				}
