7ae1d6fae20b299e22eb12c79a85e86cfd094e0f
afreenfquazi <95732654+afreenfquazi@users.noreply.github.com>
2022-11-30 21:20:58 -0600

    Add files via upload

---
+++ Deer.java
@@ -0,0 +1,16 @@
+package org.animals;
+
+public class Deer {
+	public String color= "brown";
+	public float weight = 50;
+	public int age  = 10;
+	public void isVegetarian() {
+		System.out.println("this deer is vegetarian");
+	}
+	public void canClimb() {
+		System.out.println("it cannot climb");
+	}
+	public void sound() {
+		System.out.println("it makes hoohooh sound");
+	}
+}
---
+++ VandalurZoo.java
@@ -0,0 +1,14 @@
+package org.zoo;
+
+import org.animals.Deer;
+
+public class VandalurZoo {
+
+	public static void main(String[] args) {
+	Deer dr = new Deer();
+	dr.isVegetarian();
+	dr.canClimb();
+	dr.sound();
+	}
+
+}
